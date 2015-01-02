8be46fd155d83f78170d1ce45101eede21577dcf
William Van Hevelingen <william.vanhevelingen@acquia.com>
2015-01-02 11:37:24 -0800

    Enable SSL for rabbitmq and sensu

--- puppet/modules/3am/manifests/rabbitmq.pp
+++ puppet/modules/3am/manifests/rabbitmq.pp
@@ -17,4 +17,24 @@ class 3am::rabbitmq {
       write_permission     => '.*',
   }
 
+  file {
+    '/var/lib/rabbitmq/cacert.pem':
+      owner   => 'rabbitmq',
+      group   => 'rabbitmq',
+      mode    => '0400',
+      source  => 'puppet:///modules/ssl_certs/cacert.pem';
+
+    '/var/lib/rabbitmq/cert.pem':
+      owner   => 'rabbitmq',
+      group   => 'rabbitmq',
+      mode    => '0400',
+      source  => 'puppet:///modules/ssl_certs/rabbitmq_cert.pem';
+
+    '/var/lib/rabbitmq/key.pem':
+      owner   => 'rabbitmq',
+      group   => 'rabbitmq',
+      mode    => '0400',
+      source  => 'puppet:///modules/ssl_certs/rabbitmq_key.pem';
+  }
+
 }
--- puppet/modules/3am/manifests/sensu/server.pp
+++ puppet/modules/3am/manifests/sensu/server.pp
@@ -8,24 +8,27 @@ class 3am::sensu::server {
   include 3am::sensu::handlers
 
   class { '::sensu':
-    api                  => true,
-    purge_config         => true,
-    rabbitmq_ssl         => false,
-    server               => true,
-    use_embedded_ruby    => true,
-    sensu_plugin_version => present,
-    subscriptions        => ['system_checks', 'service_rabbitmq_checks', 'service_uchiwa_checks'],
+    api                      => true,
+    purge_config             => true,
+    rabbitmq_ssl             => true,
+    rabbitmq_ssl_private_key => 'puppet:///modules/ssl_certs/sensu_key.pem',
+    rabbitmq_ssl_cert_chain  => 'puppet:///modules/ssl_certs/sensu_cert.pem',
+    server                   => true,
+    use_embedded_ruby        => true,
+    sensu_plugin_version     => present,
+    subscriptions            => ['system_checks', 'service_rabbitmq_checks', 'service_uchiwa_checks'],
   }
 
   Class['::redis'] -> Class['3am::rabbitmq'] -> Class['::sensu']
 
 
+  # TODO: the check doesn't support SSL
   # TODO: Move this somewhere more approriate
-  sensu::check { 'check_rabbitmq-cluster-health':
-    command     => '/etc/sensu/sensu-community-plugins/plugins/rabbitmq/rabbitmq-cluster-health.rb',
-    handlers    => 'slack',
-    subscribers => 'service_rabbitmq_checks'
-  }
+  # sensu::check { 'check_rabbitmq-cluster-health':
+  #  command     => '/etc/sensu/sensu-community-plugins/plugins/rabbitmq/rabbitmq-cluster-health.rb',
+  #  handlers    => 'slack',
+  #  subscribers => 'service_rabbitmq_checks'
+  # }
 
   # TODO: Move this somewhere more approriate
   sensu::check { 'check_uchiwa-health':
@@ -33,6 +36,5 @@ class 3am::sensu::server {
     handlers    => 'default',
     subscribers => 'service_uchiwa_checks'
   }
-
 
 }
